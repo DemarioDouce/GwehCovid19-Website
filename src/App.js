6c84df36b39f4cb9766f2ef2b41494ef7fe3fe55
DemarioDouce <demario.douce567@gmail.com>
2021-05-11 12:36:02 -0400

    Changes and improvements.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@
 import "./res/css/Style.css";
 // Components
 import FooterComponent from "./res/components/FooterComponent";
+// Image
+import TopSectionImage from "./res/images/TopSectionImage.jpg";
 
 function App() {
   return (
     <>
       <div className="container-fluid">
-        <section className="top-section d-flex justify-content-center">
-          <h1>Hello World.</h1>
+        <section className="top-section">
+          <div className="row">
+            <div className="col">
+              <p className="badge text-wrap">Get Well Soon, Earth!</p>
+              <h1 className="fw-bold mt-3">Let's Help</h1>
+              <h1 className="fw-bold">Recover The Earth,</h1>
+              <h1 className="fw-bold mb-3" style={{ color: "#f8c45a" }}>
+                Starts From Us!
+              </h1>
+              <p className="text-muted text-break mb-5">
+                The outbreak of the coronavirus issue or called covid-19 makes
+                the Earth grieve.
+              </p>
+              <button type="button" class="btn-primary rounded">
+                Learn More
+              </button>
+              <button type="button" class="btn-secondary rounded">
+                Watch Video
+              </button>
+            </div>
+            <div className="col">2 of 2</div>
+          </div>
         </section>
         <FooterComponent />
       </div>
